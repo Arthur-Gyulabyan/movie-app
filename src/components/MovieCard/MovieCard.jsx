3ceff101d5778bb9fb9cb86070c33cc11bbebbde
Arthur <gyulabyana@gmail.com>
2021-08-03 23:04:07 +0400

    feat(MovieCard): add reserve image url in case of non found poster image

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
+import noImage from '../../assets/images/no-image.jpg';
 
 const { Meta } = Card;
 
@@ -7,17 +8,24 @@ export default function MovieCard({ url, title, releaseDate }) {
   return (
     <Card
       hoverable
-      style={{ width: 300 }}
+      style={{ width: 280 }}
       cover={
-        <img alt="example" src={`https://image.tmdb.org/t/p/w500/${url}`} />
+        <img
+          alt="example"
+          src={url ? `https://image.tmdb.org/t/p/w500/${url}` : noImage}
+        />
       }>
       <Meta title={title} description={releaseDate} />
     </Card>
   );
 }
 
+MovieCard.defaultProps = {
+  url: '',
+};
+
 MovieCard.propTypes = {
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   title: PropTypes.string.isRequired,
   releaseDate: PropTypes.string.isRequired,
 };
