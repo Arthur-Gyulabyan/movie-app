e0dfb928f97792c2801ef56018cefbb034c2137c
Arthur <gyulabyana@gmail.com>
2021-08-03 21:04:18 +0400

    feat(helpers): add axios config file

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjQxZDU2MjczNmZmYTVmNTQyMzZiOTFlZmFhOWE3NiIsInN1YiI6IjYxMDZjZTU0N2FkMDhjMDA0NTUzMGMyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AyolfPpM2gJg9swYLV02l9Vc-ClicxdUhzblmKUUB3s`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTIwNWNjZDdmZGZjMTUzNTkxYTNhOWEzNzE1ODBlMiIsInN1YiI6IjYxMDdlNGMxOGQyMmZjMDA3NmJkNjUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2bElrr1Ds-OiC0Duu2iiP4CNipF9g1NoOe2MohCTOrQ`,
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
