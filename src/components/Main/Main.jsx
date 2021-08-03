d98d8318027c96c14c159c2e0d2f327d04a0782d
Arthur <gyulabyana@gmail.com>
2021-08-03 21:09:19 +0400

    feat(Main): add request functionality on search

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from 'react';
 import NavBar from '../NavBar/NavBar';
+import Movies from '../Movies/Movies';
 import API from '../../helpers/axios';
 
 export default function Main() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    API.get('/movie/popular?api_key=d5205ccd7fdfc153591a3a9a371580e2').then(
+      (response) => {
+        setData(response.data.results);
+      },
+    );
+  }, []);
 
   useEffect(() => {
     API.get(
-      '/movie/popular?api_key=d5205ccd7fdfc153591a3a9a371580e2&page=250',
+      `/search/movie?api_key=d5205ccd7fdfc153591a3a9a371580e2&query=${search}`,
     ).then((response) => {
-      setData(response);
+      setData(response.data.results);
     });
-  }, []);
+  }, [search]);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <>
-      <NavBar />
+      <NavBar search={search} handleSearch={handleSearch} />
+      <Movies movies={data} />
     </>
   );
 }
