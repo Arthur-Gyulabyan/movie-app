5890208ad7668e764058685f4212711da659c1cf
Arthur <gyulabyana@gmail.com>
2021-08-03 21:05:53 +0400

    feat(components): add MovieCard component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,23 @@
-import NavBar from '../NavBar/NavBar';
+import PropTypes from 'prop-types';
+import { Card } from 'antd';
 
-export default function Home() {
-  return <NavBar />;
+const { Meta } = Card;
+
+export default function MovieCard({ url, title, releaseDate }) {
+  return (
+    <Card
+      hoverable
+      style={{ width: 300 }}
+      cover={
+        <img alt="example" src={`https://image.tmdb.org/t/p/w500/${url}`} />
+      }>
+      <Meta title={title} description={releaseDate} />
+    </Card>
+  );
 }
+
+MovieCard.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+};
