800e24f769846a2d12682663509689070d29f710
Arthur <gyulabyana@gmail.com>
2021-08-06 03:23:56 +0400

    feat(LogIn): add state for inputs and connect with ProvideAuth

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,49 +1,78 @@
-import React, { PureComponent } from 'react';
+import React, { useContext, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Form, Field, ErrorMessage, withFormik } from 'formik';
 import * as yup from 'yup';
 import './LogIn.less';
+import { AuthContext } from '../ProvideAuth/ProvideAuth';
 
-class Login extends PureComponent {
-  render() {
-    return (
-      <div className="form-wrapper">
-        <Form className="form">
-          <h2 className="form-header">Log In</h2>
-          <div className="input-field-wrapper">
-            <Field
-              type="text"
-              name="username"
-              placeholder="username"
-              className="form-input"
-            />
-            <ErrorMessage name="email" />
-          </div>
+function Login() {
+  const { isAuthenticated, login } = useContext(AuthContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-          <div className="input-field-wrapper">
-            <Field
-              type="text"
-              name="password"
-              placeholder="password"
-              className="form-input"
-            />
-            <ErrorMessage name="password" />
-          </div>
-          <button type="submit"> Submit</button>
-        </Form>
-      </div>
-    );
+  const changeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
   }
+
+  return (
+    <div className="form-wrapper">
+      <Form className="form">
+        <h2 className="form-header">Log In</h2>
+        <div className="input-field-wrapper">
+          <Field
+            type="text"
+            name="username"
+            placeholder="username"
+            className="form-input"
+            value={username}
+            onChange={changeUsername}
+          />
+          <ErrorMessage name="email" />
+        </div>
+
+        <div className="input-field-wrapper">
+          <Field
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="form-input"
+            value={password}
+            onChange={changePassword}
+          />
+          <ErrorMessage name="password" />
+        </div>
+        <button type="submit" className="submit-btn" onClick={login}>
+          {' '}
+          Submit
+        </button>
+      </Form>
+    </div>
+  );
 }
 
-const LoginWrapper = Login;
-
 const LoginValidation = yup.object().shape({
-  email: yup.string().email().required(),
+  username: yup
+    .string()
+    .min(2)
+    .max(12)
+    .matches('^[a-zA-Z]+$', 'only letters')
+    .required(),
   password: yup
     .string()
     .min(8)
     .max(16)
-    .matches('^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$')
+    .matches(
+      '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$',
+      'at least 1 uppercase, 1 digit',
+    )
     .required(),
 });
 
@@ -52,4 +81,4 @@ export default withFormik({
     setTimeout(() => setSubmitting(false), 3 * 1000);
   },
   validationSchema: LoginValidation,
-})(LoginWrapper);
+})(Login);
