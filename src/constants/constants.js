385be540e32c54fae344db49538b74ae21ebb9a8
Arthur <gyulabyana@gmail.com>
2021-08-07 14:39:37 +0400

    feat(constants): add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const READ_ACCESS_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTIwNWNjZDdmZGZjMTUzNTkxYTNhOWEzNzE1ODBlMiIsInN1YiI6IjYxMDdlNGMxOGQyMmZjMDA3NmJkNjUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2bElrr1Ds-OiC0Duu2iiP4CNipF9g1NoOe2MohCTOrQ';
+export const API_KEY = 'd5205ccd7fdfc153591a3a9a371580e2';
+export const POPULAR_URL =
+  '/movie/popular?api_key=d5205ccd7fdfc153591a3a9a371580e2';
+export const SEARCH_URL =
+  '/search/movie?api_key=d5205ccd7fdfc153591a3a9a371580e2';
