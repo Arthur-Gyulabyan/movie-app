1c2bc480b346ecf2621af78e1e185d40447d56e6
Arthur <gyulabyana@gmail.com>
2021-08-08 20:47:04 +0400

    feat(cosntants): add image link to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,3 +6,4 @@ export const POPULAR_URL =
   '/movie/popular?api_key=d5205ccd7fdfc153591a3a9a371580e2';
 export const SEARCH_URL =
   '/search/movie?api_key=d5205ccd7fdfc153591a3a9a371580e2';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
