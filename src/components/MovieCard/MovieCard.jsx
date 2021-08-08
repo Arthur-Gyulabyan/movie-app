356ce07091196b9b569668f18f84b660f14fc91e
Arthur <gyulabyana@gmail.com>
2021-08-08 20:48:10 +0400

    feat(MovieCard): add like icons and get handlers for them

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,19 +1,33 @@
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
+import { HeartOutlined, HeartFilled } from '@ant-design/icons';
 import './MovieCard.less';
 import noImage from '../../assets/images/no-image.jpg';
+import { IMAGE_URL } from '../../constants/constants';
 
 const { Meta } = Card;
 
-export default function MovieCard({ url, title, releaseDate }) {
+const isFavorite = (id, favorites) => {
+  return favorites.some((el) => el.id === id);
+};
+
+export default function MovieCard({
+  url,
+  title,
+  releaseDate,
+  favorites,
+  id,
+  handleLike,
+  handleUnlike,
+}) {
   return (
     <Card
       hoverable
-      cover={
-        <img
-          alt="example"
-          src={url ? `https://image.tmdb.org/t/p/w500/${url}` : noImage}
-        />
+      cover={<img alt="example" src={url ? `${IMAGE_URL}${url}` : noImage} />}
+      actions={
+        isFavorite(id, favorites)
+          ? [<HeartFilled key="unlike" onClick={() => handleUnlike(id)} />]
+          : [<HeartOutlined key="like" onClick={() => handleLike(id)} />]
       }>
       <Meta title={title} description={releaseDate} />
     </Card>
@@ -30,4 +44,9 @@ MovieCard.propTypes = {
   url: PropTypes.string,
   title: PropTypes.string,
   releaseDate: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  handleLike: PropTypes.func.isRequired,
+  handleUnlike: PropTypes.func.isRequired,
+  favorites: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object]))
+    .isRequired,
 };
