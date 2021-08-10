fdf72f7ec18cacccaa7fb3f8a3516fc6147d2bc1
Arthur <gyulabyana@gmail.com>
2021-08-11 01:38:14 +0400

    feat(ProvideAuth): add save user data to local storage functionality

--- src/components/ProvideAuth/ProvideAuth.jsx
+++ src/components/ProvideAuth/ProvideAuth.jsx
@@ -1,18 +1,29 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { saveData, getData } from '../../helpers/localStorage';
 
 export const AuthContext = React.createContext({});
 
 export default function ProvideAuth({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    Boolean(getData('isAuth')),
+  );
+  const [currentUser, setCurrentUser] = useState(getData('currentUser'));
 
-  const login = useCallback(() => {
+  const login = useCallback((values) => {
+    setCurrentUser(values);
     setIsAuthenticated(true);
   }, []);
 
   const logout = useCallback(() => {
+    setCurrentUser(null);
     setIsAuthenticated(false);
   }, []);
+
+  useEffect(() => {
+    saveData('isAuth', isAuthenticated);
+    saveData('currentUser', currentUser);
+  }, [isAuthenticated, currentUser]);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
